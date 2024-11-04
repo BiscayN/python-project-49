736ddb0d80ff971e3468c1a594630d8881d8d911
Maxim Nazarov <nazarov010203@yandex.ru>
2024-11-04 04:11:49 +0300

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,7 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(number):
     if number == 2:
         return True
-    for i in range(3, int(number ** 0.5) + 1):
+    for i in range(2, int(number ** 0.5) + 2):
         if number % i == 0:
             return False
     return True
